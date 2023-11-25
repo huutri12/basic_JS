3c643f482dfc452fff7cdb091ff7a020dc367af5
huutri12 <tri_1951220112@dau.edu.vn>
2023-11-25 21:17:01 +0700

    first commit

---
+++ javascript/day1/array.js
@@ -0,0 +1,13 @@
+
+let a = {
+    name: 'Eric',
+    age: 25
+}; //key:value
+
+console.log('check data object: ', a);
+console.log('my name\'s:', a.name)
+console.log('my age\'s:', a.age)
+
+//arr = [item1, item2, item3]
+let b = ['MU', 'Chelsea', 'Liverpool'];
+console.log('typeof b', typeof b, b)
---
+++ javascript/day1/demo.js
@@ -0,0 +1,8 @@
+console.log('hello world ')
+let a = 5;
+let b = '5';
+
+console.log(' compare a, b: ', 'a = ', a, 'b = ', a, ' result: ', a == b)
+
+// === type, value convention
+// == value
